bde2f9539d3c1c2a16f0a7f16ac3df1e69b015ba
Vardan Karapetian <vardankaradas@gmail.com>
2022-01-15 13:38:55 +0300

    Remove unnecessary libraries

--- PCBuilder/Domain/IComponentsService.cs
+++ PCBuilder/Domain/IComponentsService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using PCBuilder.DataAccess.Entities;
 using PCBuilder.Domain.Models;
--- PCBuilder/Domain/IOrderService.cs
+++ PCBuilder/Domain/IOrderService.cs
@@ -1,7 +1,4 @@
-﻿using PCBuilder.DataAccess.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using PCBuilder.Domain.Models;
 
